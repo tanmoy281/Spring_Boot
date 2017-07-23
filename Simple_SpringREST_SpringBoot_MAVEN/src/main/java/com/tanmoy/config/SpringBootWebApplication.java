58c8bd2e55dac7634830e086625e85aa7ec906b1
tanmoy281 <tanmoy.banerjee@outlook.in>
2017-07-23 20:49:41 +0530

    congig file

---
+++ Simple_SpringREST_SpringBoot_MAVEN/src/main/java/com/tanmoy/config/SpringBootWebApplication.java
@@ -0,0 +1,21 @@
+package com.tanmoy.config;
+
+import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication(scanBasePackages={"com.tanmoy"})
+
+public class SpringBootWebApplication extends SpringBootServletInitializer{
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+}
