c99f0ab4047297fc0857f5573e69cf677ef1a55e
tanmoy281 <tanmoy.banerjee@outlook.in>
2017-07-24 00:12:03 +0530

    Adding log4j and enabling spring hot swapping

--- Simple_SpringREST_SpringBoot_MAVEN/src/main/java/com/tanmoy/controller/HelloRest.java
+++ Simple_SpringREST_SpringBoot_MAVEN/src/main/java/com/tanmoy/controller/HelloRest.java
@@ -1,5 +1,6 @@
 package com.tanmoy.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/simaple")
 public class HelloRest {
 
+	private static Logger log = Logger.getLogger(HelloRest.class);
+	
 	@RequestMapping(value="/greeting", method=RequestMethod.GET, produces="application/json")
 	public Employee ping(){
-		
+		log.info("Logger enabled: Entering rest method \n\n");
 		Employee e =new Employee();
 		
 		e.setName("Tanmoy");
-		e.setAddress("Kolkata");
+		e.setAddress("Hyderabad");
+		
+		log.info("Logger enabled: Exiting rest method");
 		
 		return e;
 	}
